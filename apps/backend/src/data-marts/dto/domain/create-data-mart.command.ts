74d3c3aee31809761c7b10ddbfbf86066cd20e9a
Antony Ishchenko <141525289+AntonyIshchenko@users.noreply.github.com>
2025-06-11 16:57:41 +0300

    Test branch (#2)
    * test3
    
    * test back 4
    
    * fix workflow
    
    * fix prettier config
    
    * some fixes prettier

--- apps/backend/src/data-marts/dto/domain/create-data-mart.command.ts
+++ apps/backend/src/data-marts/dto/domain/create-data-mart.command.ts
@@ -3,8 +3,6 @@ import { DataStorageType } from '../../enums/data-storage-type.enum';
 export class CreateDataMartCommand {
   constructor(
     public readonly title: string,
-    public readonly title2: any,
-    public readonly title3: any,
     public readonly storage: DataStorageType
   ) {}
 }
