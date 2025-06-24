c9ad0441dccb02adcecd772ab6c71818c5353259
Antony Ishchenko <antony.ishchenko@gmail.com>
2025-06-24 13:58:27 +0300

    add base to astro config

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
-  site: 'https://antonyishchenko.github.io/owox-data-marts-fork',
+  // site: 'https://antonyishchenko.github.io/owox-data-marts-fork',
+  base: '/owox-data-marts-fork',
   integrations: [
     starlight({
       title: 'OWOX Docs',
